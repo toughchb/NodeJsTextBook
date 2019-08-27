2a8471bf4aab8e1d5173751b5ea75f6366d46dfc
ggang2000 <ggang2000@gmail.com>
2019-08-27 19:12:15 +0900

    ch9 nodebird

--- ch9/nodebird/routes/page.js
+++ ch9/nodebird/routes/page.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
+
 const router = express.Router();
 
 
-router.get('/profile',(req,res) => {
+router.get('/profile',isLoggedIn,(req,res) => {
   res.render('profile',{title:'내 정보 - NodeBird', user: null});
 });
 
 router.get('/join',(req,res) => {
   res.render('join',{
   title:'회원가입 - NodeBird',
-  user: null,
+  user: req.user,
   joinError:req.flash('joinError'),
   });
 });
@@ -17,7 +19,7 @@ router.get('/join',(req,res) => {
 router.get('/',(req,res) => {
   res.render('main',{title:'NodeBird',
   twits: [],
-  user: null,
+  user: req.user,
   joinError:req.flash('joinError'),
   });
 });
